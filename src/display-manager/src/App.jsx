b0e0684c41bb90daed68a9f31067b0b5d4a08f64
BenjaminJorgensen <benjamin@jorgensens.id.au>
2023-08-29 11:12:50 +1000

    Created file to open websocket

--- src/display-manager/src/App.jsx
+++ src/display-manager/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import  ImgPod  from './Components/ImgPod.jsx'
+import ImgPod from './Components/ImgPod.jsx'
+import connect_to_server from './Components/ServerConnect.jsx'
+
+//Just for testing, eventually will create a websocket to the node server
+connect_to_server()
 
 /* Just as a note: Images need to be imported so that React can use
 *  Absolute paths during complile
