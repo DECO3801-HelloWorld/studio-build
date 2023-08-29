39e1437450127a63fa46f9abf7f3990b0fd701b7
BenjaminJorgensen <benjamin@jorgensens.id.au>
2023-08-29 21:01:08 +1000

    Added the image rendering system
    Data can now be passed in at the App.jsx file to be rendered in image
    containers. Created colours.json for the list of hex values that are
    commonly used in display-manager. Added some more styling to App.css.

--- src/display-manager/src/App.jsx
+++ src/display-manager/src/App.jsx
@@ -1,19 +1,60 @@
 import { useState } from 'react'
-import './App.css'
 import ImgPod from './Components/ImgPod.jsx'
-import connect_to_server from './Components/ServerConnect.jsx'
+import colours from './Components/colours.json'
+
+//Importing all images - We will get these by web socket in future
+import cat from './images/cat.jpg'
+import zebra from './images/zebra.jpg'
+import bird from './images/bird.jpg'
+import nut from './images/squirrel.jpg'
+
+import connect_to_server from './Components/ServerConnect.jsx' //Not implemented
+
+//Application styling - ( ImgPod styling found at ./Components/ImgPod.css)
+import './App.css'
+
 
 //Just for testing, eventually will create a websocket to the node server
 connect_to_server()
 
-/* Just as a note: Images need to be imported so that React can use
-*  Absolute paths during complile
+/* Simulating user data received from websocket */
+const userData1 = {
+	userId: 1,
+	style: colours.red,
+	name: "Benjamin",
+	img: zebra,
+}
+
+const userData2 = {
+	userId: 2,
+	style: colours.blue,
+	name: "Kristian",
+	img: cat,
+}
+
+const userData3 = {
+	userId: 3,
+	style: colours.green,
+	name: "Tad",
+	img: bird,
+}
+
+const userData4 = {
+	userId: 4,
+	style: colours.purple,
+	name: "Benjamin Again",
+	img: nut,
+}
+
+/* This is where the application is rendered
 */
 export default function App() {
 	return (
 		<>
-		{/* Five zebra's because why not */}
-		<ImgPod/>
+		<ImgPod data={userData1}/>
+		<ImgPod data={userData2}/>
+		<ImgPod data={userData3}/>
+		<ImgPod data={userData4}/>
 		</>
 	)
 }
--- src/display-manager/src/Components/ImgPod.jsx
+++ src/display-manager/src/Components/ImgPod.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import logo from '../images/squirrel.jpg'
 import '../App.css'
 import './ImgPod.css'
 
 /* The ImgPod(s) are an element that are going to render the user's image in a
 * colourful frame. Eventually it'll take arguments from App.jsx about the
 * name and associated id/colour of the bubble. 
-*
-* */
-export default function ImgPod() {
+*/
+export default function ImgPod({ data }) {
+	const podStyle = {
+		backgroundImage: `url('${data.img}')`,
+	}
 	return (
-		<div className="imgPod">
-			<p className="user-name"> Benjamin Jorgensen </p>
-			<div className="img-content" src={logo} alt="whoops"/>
+		<div className="imgPod" style={data.style}>
+			<p className="user-name" style={data.style}> {data.name} </p>
+			<div className="img-content" style={podStyle} alt="whoops"/>
 		</div>
 	)
 }
