ff1c974e2a76f6a679c11ec6161bf3703d1d16f1
BenjaminJorgensen <benjamin@jorgensens.id.au>
2023-08-31 21:15:10 +1000

    Added the ability to add ImgPod's with the spacebar
    This is used for testing only

--- src/display-manager/src/App.jsx
+++ src/display-manager/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, {useState, useEffect} from 'react'
 import ImgPod from './Components/ImgPod.jsx'
 import colours from './Components/colours.json'
 
@@ -7,29 +7,26 @@ import cat from './images/cat.jpg'
 import zebra from './images/zebra.jpg'
 import bird from './images/bird.jpg'
 import nut from './images/squirrel.jpg'
+import { v4 as uuidv4 } from 'uuid';
 
 import connect_to_server from './Components/ServerConnect.jsx' //Not implemented
 
 //Application styling - ( ImgPod styling found at ./Components/ImgPod.css)
 import './App.css'
 
-
-//Just for testing, eventually will create a websocket to the node server
-connect_to_server()
-
 /* Simulating user data received from websocket */
 const userData1 = {
 	userId: 1,
 	style: colours.red,
 	name: "Benjamin",
-	img: zebra,
+	img: cat,
 }
 
 const userData2 = {
 	userId: 2,
 	style: colours.blue,
 	name: "Kristian",
-	img: cat,
+	img: zebra,
 }
 
 const userData3 = {
@@ -46,15 +43,49 @@ const userData4 = {
 	img: nut,
 }
 
+//Just for testing, eventually will create a websocket to the node server
+connect_to_server()
+
+
 /* This is where the application is rendered
 */
 export default function App() {
+	const [ElmImgPod, setElmImgPod] = useState([]);
+	const [count, setCount] = useState(0);
+	const users = [userData1, userData2, userData3, userData4];
+
+	useEffect(() => {
+		const handleKeyDown = (event) => {
+			if (event.code == "Space") {
+				create_element(users[count % 4]);
+			}
+		}
+		document.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown);
+		}
+	})
+
+
+	function create_element(user_data) {
+		setCount(count + 1);
+		setElmImgPod((currentImgPods) => {
+			return [
+				...currentImgPods,
+				{id: uuidv4(), data: user_data },
+			]
+		})
+	}
+
 	return (
 		<>
-		<ImgPod data={userData1}/>
-		<ImgPod data={userData2}/>
-		<ImgPod data={userData3}/>
-		<ImgPod data={userData4}/>
+		<h1>Press [SPACE] to add image</h1>
+		{ElmImgPod.map(vibe => {
+			return (
+				<ImgPod key={vibe.id} data={vibe.data}/>
+			)
+		})}
 		</>
 	)
 }
