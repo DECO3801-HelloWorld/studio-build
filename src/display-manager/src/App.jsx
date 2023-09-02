29b0c4837bdf95f1dd4772986f8367913445875f
BenjaminJorgensen <benjamin@jorgensens.id.au>
2023-09-03 01:39:48 +1000

    Implemented the uploading of images to the display-manager
    If you want to know everything that happened, please ask me, I can't
    write this all out.
    Created basic skelleton client and server to test web sockets

--- src/display-manager/src/App.jsx
+++ src/display-manager/src/App.jsx
@@ -1,67 +1,39 @@
 import {useState, useEffect} from 'react'
-import ImgPod from './Components/ImgPod.jsx'
-import SplashScreen from './Components/SplashScreen.jsx'
-import colours from './Components/colours.json'
-
-//Importing all images - We will get these by web socket in future
-import cat from './images/cat.jpg'
-import zebra from './images/zebra.jpg'
-import bird from './images/bird.jpg'
-import nut from './images/squirrel.jpg'
+import io from 'socket.io-client'
 import { v4 as uuidv4 } from 'uuid';
 
-import connectToServer from './Components/ServerConnect.jsx' //Not implemented
+import colours from './Components/colours.json'
+import ImgPod from './Components/ImgPod.jsx'
+import SplashScreen from './Components/SplashScreen.jsx'
 
 //Application styling - ( ImgPod styling found at ./Components/ImgPod.css)
 import './App.css'
 
-/* Simulating user data received from websocket */
-const imgPodData1 = {
-	userId: 1,
-	style: colours.red,
-	name: "Benjamin",
-	img: cat,
-}
-
-const imgPodData2 = {
-	userId: 2,
-	style: colours.blue,
-	name: "Kristian",
-	img: zebra,
-}
-
-const imgPodData3 = {
-	userId: 3,
-	style: colours.green,
-	name: "Tad",
-	img: bird,
-}
-
-const imgPodData4 = {
-	userId: 4,
-	style: colours.purple,
-	name: "Benjamin Again",
-	img: nut,
-}
-const imgPodsTemplate = [imgPodData1, imgPodData2, imgPodData3, imgPodData4];
-
-//Just for testing, eventually will create a websocket to the node server
-connectToServer()
+const socket = io.connect("http://localhost:3001");
 
 /* This is where the application is rendered */
 export default function App() {
 	//Controlling the state - If you need to change these variables
 	//Do so with the setX functions
 	const [images, setImages] = useState([]);
-	const [count, setCount] = useState(0);
+
+	//If an image is pushed onto the client
+	useEffect(() => {
+		socket.on("download_img", (imgPacket) => {
+			//Check for command in future
+			console.log("image received")
+			addImage(imgPacket);
+		})
+		return () => {
+			socket.off("download_img");
+		}
+	}, []);
 
 	/* Will add images to the webpage - call with user data as seen above */
-	function addImage(userData) {
+	function addImage(imgPacket) {
+		Object.assign(imgPacket, {style: colours[Math.floor(Math.random() * 5)]}) //TODO: Map this properly
 		setImages((currentImages) => {
-			return [
-				...currentImages,
-				{id: uuidv4(), data: userData },
-			]
+			return [ ...currentImages, {id: uuidv4(), data: imgPacket}, ]
 		})
 	}
 
@@ -70,21 +42,25 @@ export default function App() {
 		setImages(() => {return []})
 	}
 
-	/* Binds [Space] and [Backspace] to add and remove images respectivly */
+	/* Remove all images from a user with the ID given -- called when user disconnect */
+	function removeUser(userId) {
+		setImages((currentImages) => {
+			return currentImages.filter((image) => image.data.userId !== userId)
+		})
+	}
+
+	/* Binds [Backspace] to remove images on screen*/
 	useEffect(() => {
 		const handleKeyPress = (event) => {
-			if (event.code == "Space") {
-				event.preventDefault(); //Stop normal space activity
-				addImage(imgPodsTemplate[count % 4]); //Add an image (this will be from websocket in future)
-				setCount(count + 1); //Remember how many images there are
-			}
-			if (event.code == "Backspace") {
-				event.preventDefault();
-				removeAllImages();
-				setCount(0);
+			switch (event.code) {
+				case "Backspace":
+					removeAllImages();
+					setCount(0);
+					break;
+				default:
+					break;
 			}
 		}
-
 		document.addEventListener('keydown', handleKeyPress)
 		return (() => {document.removeEventListener('keydown', handleKeyPress)})
 	});
