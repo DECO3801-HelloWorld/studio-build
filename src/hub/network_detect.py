2a8e1e411d4cf222501972c7fcca657d214b7704
Awidat17 <mlamtadi1705@gmail.com>
2023-09-06 04:01:09 +0100

    added pinging and threads to detect disconnects

--- src/hub/network_detect.py
+++ src/hub/network_detect.py
@@ -1,23 +1,48 @@
-from scapy.all import ARP, sniff
+import os
+import threading
+import time
 import datetime
 
-connected_devices = {}
+from scapy.all import ARP, sniff
+
+connected_devices = {} # {ip:mac}
+
+# lock for connected_devices dict
+lock = threading.Lock()
 
 def process_packet(packet):
-    if ARP in packet:
+    if ARP in packet and packet[ARP].op == 1: # ARP Request
         ip = packet[ARP].psrc
         mac = packet[ARP].hwsrc
-        packet[ARP].show()
 
-        if packet[ARP].op == 1: # ARP Request
+        with lock: # Get lock
             if ip not in connected_devices:
                 connected_devices[ip] = mac
-                #print(f"New device connected:\nIP: {ip}\nMAC: {mac}\nTime: {datetime.datetime.now()}")
+                print(f"New device connected:\nIP: {ip}\nTime: {datetime.datetime.now()}")
 
-        elif packet[ARP].op == 2: # ARP Reply
-            if ip in connected_devices:
+def device_ping():
+    while True:
+        tmp_connected = {}
+        with lock: # Get lock
+            tmp_connected = dict(connected_devices) # copy of dict
+        for ip, mac in tmp_connected.items():
+            response = os.system(f"ping -c 1 {ip}") # Ping device
+            if response != 0:
+                print(f"Device disconnected:\nIP: {ip}\nTime: {datetime.datetime.now()}")
                 del connected_devices[ip]
-                #print(f"Device disconnected:\nIP: {ip}\nTime: {datetime.datetime.now()}")
 
-# sniffing for ARP packets on the network
-sniff(filter="arp", prn=process_packet, store=0)
+        time.sleep(10) # 10s
+
+# ARP packet processing thread
+arp_thread = threading.Thread(target=lambda: sniff(filter="arp", prn=process_packet, store=0))
+
+# Device ping thread
+ping_thread = threading.Thread(target=device_ping)
+
+# Start Threads
+arp_thread.start()
+ping_thread.start()
+
+# Finish Threads
+arp_thread.join()
+ping_thread.join()
