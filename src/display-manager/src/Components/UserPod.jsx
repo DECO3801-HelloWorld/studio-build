5b20988026bef1e3467cfc0c798f2b6dafafcea3
BenjaminJorgensen <benjamin@jorgensens.id.au>
2023-09-13 17:09:03 +1000

    Added css and correct styling to UserPod

--- src/display-manager/src/Components/UserPod.jsx
+++ src/display-manager/src/Components/UserPod.jsx
@@ -1,13 +1,13 @@
 import '../App.css'
-import './ImgPod.css'
+import './UserPod.css'
 
 /* The UserPod(s) are an element that are going to render a small icon for each
 *  user
 */
 export default function UserPod({ data }) {
 	return (
-		<div className="userPod" style={data.style} height="5%" width="5%" 
-			position="absolute" right="0px" top="0px">
+		<div className="userPod" style={data.style} >
+		userPod
 		</div>
 	)
 }
