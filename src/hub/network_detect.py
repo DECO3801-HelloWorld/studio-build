e056811ab4a5f2eece0e5ffe416b5fccd1f82007
Awidat17 <mlamtadi1705@gmail.com>
2023-10-11 02:44:33 +0100

    fixed socketproblem

--- src/hub/network_detect.py
+++ src/hub/network_detect.py
@@ -15,7 +15,7 @@ from user_manager import UserManager
 connected_devices = {} # {mac:ip}
 
 # create socket client
-sio = socketio.AsyncClient()
+sio = socketio.Client()
 
 # lock for connected_devices dict
 lock = threading.Lock()
@@ -38,14 +38,14 @@ def stop_capture(packet):
     return exit_flag
 
 def msg_format(ip):
-    int_ip = int(ipaddress.IPv4Address(ip))
-    with lock:
-        usr_count = len(connected_devices)    
+    global connected_devices
 
+    int_ip = int(ipaddress.IPv4Address(ip))
+    usr_count = len(connected_devices)
     #msg_str = '{' + str(int_ip) + ':' + str(usr_count) + '}'
     msg_json = {int_ip:usr_count} 
-
-    return msg_json  
+    print(msg_json)
+    return msg_json
 
 def process_packet(packet):
     global connected_devices
@@ -58,7 +58,7 @@ def process_packet(packet):
             if mac not in connected_devices:
                 user_m.add_user(mac, ip)
                 connected_devices[mac] = ip
-                print(f"New device connected:\nIP: {ip}\nTime: {datetime.datetime.now()}")
+                print(f"New device connected:\n IP: {ip}")
                 sio.emit('new_connection', msg_format(ip)) # send new connection ip to server
 
 def device_ping():
@@ -70,24 +70,23 @@ def device_ping():
         with lock: # Get lock
             tmp_connected = dict(connected_devices) # copy of dict
         for mac, ip in tmp_connected.items():
-            #with contextlib.redirect_stdout(None): # suppress ping msg
             response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1") # Ping device
             if response != 0:
-                print(f"Device disconnected:\nIP: {ip}\nTime: {datetime.datetime.now()}")
+                print(f"Device disconnected:\n IP: {ip}")
                 del connected_devices[mac]
-                sio.emit('lost_conneciton', msg_format(ip)) # send disconnected ip to server
+                sio.emit('lost_connection', msg_format(ip)) # send disconnected ip to server
 
-        time.sleep(10) # 10s
+        time.sleep(5) # seconds
 
 def close_cmd():
     global connected_devices
+    global exit_flag
     # Wait for user input to exit the program
     while True:
         user_input = input()
         if user_input.lower() == "end":
             exit_flag = True
             connected_devices = {}
-            sio.disconnect()
             break
 # -------------------- Define & Start Threads -----------------
 
@@ -107,27 +106,28 @@ close_thread.start()
 
 # socket comminications
 @sio.event
-async def connect():
-    print('connected to server')
+def connect():
+    print(' - - connected to server - - ')
 
 @sio.event
-async def disconnect():
+def disconnect():
     print('disconnected from server')
 
-async def main():
+def main():
     # get local hostname
     hostname = socket.gethostname()
     # get ip address
     ip_address = socket.gethostbyname(hostname)
-    print(f'IP: {ip_address}')
-    port = os.environ.get("PORT", "3001")
+    port = os.environ.get("PORT", "3002")
     try:
-        await sio.connect(ip_address + port)
+        print(f"{ip_address}:{port}")
+        sio.connect(f'http://{ip_address}:{port}') # +':' + port)
     except Exception as e:
         print(f'Error Msg: {e}')
 
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
 
 # ----------------------
 
@@ -135,5 +135,7 @@ if __name__ == '__main__':
 arp_thread.join()
 ping_thread.join()
 close_thread.join()
+
+sio.disconnect()
 
 print("COMPLETED")
