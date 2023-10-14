f03150bb585b90bf25b84e213bcb599b68af8139
BenjaminJorgensen <benjamin@jorgensens.id.au>
2023-10-14 19:52:33 +1000

    Fixed some styling thing

--- src/display-manager/src/Components/ImgPod.jsx
+++ src/display-manager/src/Components/ImgPod.jsx
@@ -59,9 +59,10 @@ export default function ImgPod({ data, setImages }) {
 			area: img.current.width * img.current.height,
 			ratio: img.current.width / img.current.height //L + ratio
 		}
+		resizeImages({setImages})
 		setTimeout(() => {
 			resizeImages({setImages})
-		}, 5)
+		}, 500)
 	}
 
 	//This is fucked but lets just ignore it for now
